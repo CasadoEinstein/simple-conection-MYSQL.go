69c20b5cfe690de11234637c0dc85eb69379c1b1
Pedro Lucas <88470105+CasadoEinstein@users.noreply.github.com>
2022-06-18 22:34:33 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:10811805aA*#@tcp(127.0.0.1:3306)/test table")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	insert, err := db.Query("INSERT INTO testable2 VALUES(23)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+	fmt.Println("yay, values added")
+}
